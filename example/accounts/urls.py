dcb7dda0d441a20250c56b752aebbfbf73b7a04a
kazungu <kazungu61@gmail.com>
2020-08-17 13:55:59 +0300

    auth forms

--- example/accounts/urls.py
+++ example/accounts/urls.py
@@ -3,6 +3,6 @@ from django. urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'Home'),
+    path('', views.index, name = 'home'),
     path('accounts/sign_up/',views.sign_up,name="sign-up")
 ]
